bc6350207217ac2929156172a3c925e2e90e4a6e
Wilfried-Tech <wilfriedtech.dev@gmail.com>
2025-05-28 17:17:54 +0100

    fix: update get_or_create_model method to use Type for model parameter and optimize tag creation

--- questions/management/commands/create_default_questions.py
+++ questions/management/commands/create_default_questions.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from typing import Type
 
 from django.core.management import BaseCommand
 from django.db import transaction, models
@@ -12,7 +13,7 @@ class Command(BaseCommand):
 
     data_dir = Path(__file__).resolve().parent / 'data'
 
-    def get_or_create_model(self, model: models.Model, defaults=None, **kwargs):
+    def get_or_create_model(self, model: Type[models.Model], defaults=None, **kwargs):
         obj, created = model.objects.get_or_create(**kwargs, defaults=defaults)
         if created:
             self.stdout.write(self.style.SUCCESS(f'Creating Model {model._meta.verbose_name} {obj}'))
@@ -33,13 +34,13 @@ class Command(BaseCommand):
             self.stdout.write(f'Importing questions from {file.name}\n')
 
             domain, _ = self.get_or_create_model(Domain, name=data['domain'].strip())
-            category, _ = self.get_or_create_model(Category, name=data['category'].strip(), defaults={'domain': domain})
-            criteria, _ = self.get_or_create_model(Criteria, name=data['criteria'].strip(),
-                                                   defaults={'category': category})
+            category, _ = self.get_or_create_model(Category, name=data['category'].strip(), domain=domain)
+            criteria, _ = self.get_or_create_model(Criteria, name=data['criteria'].strip(), category=category)
 
             tags_dict = {}
             for tag in data.get('tags', []):
-                obj, _ = self.get_or_create_model(Tag, name=tag, defaults={'criteria': criteria})
+                obj, _ = self.get_or_create_model(Tag, criteria__category__domain=domain, name=tag,
+                                                  defaults={'criteria': criteria})
                 tags_dict[tag] = obj
 
             for question_data in data.get('questions', []):
@@ -60,8 +61,12 @@ class Command(BaseCommand):
                 question.tags.set([tags_dict[tag] for tag in question_data['tags'] if tag in tags_dict])
 
                 if question.is_multiple_choice or question.is_unique_choice:
+                    choices = []
                     for choice_data in question_data['choices']:
-                        Choice.objects.create(question=question, text=choice_data['text'], is_correct=choice_data['is_correct'])
+                        choice = Choice(question=question, text=choice_data['text'],
+                                              is_correct=choice_data['is_correct'])
+                        choices.append(choice)
+                    Choice.objects.bulk_create(choices)
                         # self.get_or_create_model(
                         #     Choice,
                         #     defaults={'question': question},
