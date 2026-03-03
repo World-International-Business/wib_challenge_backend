e6301592273c8081bfc2678e7f2187ad13c62c4e
francktitus237 <tchoutafranck6@gmail.com>
2026-03-03 12:04:24 +0100

    chore: add French help text and error logging to wait_for_db command
    - Added French help text "Attend que la base de donne soit disponible"
    - Added database connection selection (connection['default'])
    - Added error logging with styled output showing database error details
    - Added French comment for logging importance

--- apps/core/management/commands/wait_for_db.py
+++ apps/core/management/commands/wait_for_db.py
@@ -8,14 +8,19 @@ from django.db.utils import OperationalError
 class Command(BaseCommand):
     """Django command to wait for database to be available"""
 
+    help = "Attend que la base de donne soit disponible"
+
     def handle(self, *args, **options):
         self.stdout.write('Waiting for database...')
         db_conn = None
         while not db_conn:
             try:
+                connection = connection['default']
                 connection.ensure_connection()
                 db_conn = True
             except OperationalError:
+                #log important ;voir l erreur exacte
+                self.stdout.write(str(self.style.ERROR(f'Databaseerror:{e}')))
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
