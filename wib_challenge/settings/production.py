db0c15c63e54b012337d758be5a2e5a021d1c2ad
Franck Tchouta <franck.tchouta@worldwide-international.business>
2026-07-22 11:45:46 +0200

    feat: supprimer les options de configuration MySQL spécifiques de la base de données de production

--- wib_challenge/settings/production.py
+++ wib_challenge/settings/production.py
@@ -10,12 +10,6 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, conn_health_checks=True)
 
-if not DATABASES['default']['ENGINE'].endswith('sqlite'):
-    DATABASES['default']['OPTIONS'] = {
-        'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        'charset': 'utf8mb4',
-    }
-
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 SECURE_HSTS_SECONDS = 0
