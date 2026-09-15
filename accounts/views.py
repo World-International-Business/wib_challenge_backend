3ac3eca311e961fb8fbe46dd603c5a7b99037809
Franck Tchouta <franck.tchouta@worldwide-international.business>
2026-09-15 20:26:43 +0200

    Profile: retire les emojis et elargit le formulaire sur desktop

--- accounts/views.py
+++ accounts/views.py
@@ -17,7 +17,7 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user, backend='django.contrib.auth.backends.ModelBackend')
-            messages.success(request, "Inscription réussie ! Bienvenue 😊")
+            messages.success(request, "Inscription réussie ! Bienvenue.")
             return redirect('challenge_evaluation')
     else:
         form = UserRegisterForm()
